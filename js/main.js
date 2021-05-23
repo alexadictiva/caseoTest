f9d03a47431fa9e8e6f044ce61abea2caf6a6f9f
Libia Perez <alexandra220790@gmail.com>
2021-05-23 15:20:05 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+dropdownButton = document.getElementById("dropdownButton");
+var span1 = document.getElementById("span1");
+var span2 = document.getElementById("span2");
+var span3 = document.getElementById("span3");
+
+function crossMenu() {
+  if (dropdownButton.checked) {
+    span1.classList.add("span1");
+    span2.classList.add("span2");
+    span3.classList.add("span3");
+  } else {
+    span1.classList.remove("span1");
+    span2.classList.remove("span2");
+    span3.classList.remove("span3");
+  }
+}
